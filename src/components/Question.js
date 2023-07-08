facd2721451d6dd577e0cc3413c644dfef0b1725
anissalopez <anissa965@gmail.com>
2023-07-08 15:56:09 -0400

    useEffects practice

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
+  useEffect(()=>{
+
+
+  if (timeRemaining === 0){
+      setTimeRemaining(10)
+      onAnswered(false)
+    }
+    
+  const interval = setTimeout(() =>{
+          setTimeRemaining(prevTimeRemaining => {
+            return prevTimeRemaining - 1
+          });
+        }, 1000)
+
+  return function cleanUp(){
+      clearTimeout(interval)
+    }
+   })
+
   function handleAnswer(isCorrect) {
+    
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
   const { id, prompt, answers, correctIndex } = question;
+
+
 
   return (
     <>
